23bd2795b1fa798913d934a3487b3b493e28b339
KybalionX <nicolaseduardoflorez1@gmail.com>
2023-08-23 20:17:30 -0500

    fix(turn.service): changes hostname to computername

--- src/services/turn.service.ts
+++ src/services/turn.service.ts
@@ -3,7 +3,7 @@ import { api } from 'src/boot/axios';
 import { Turn } from 'src/models/turn.model';
 
 export const insertNewTurn = async (turn: Turn): Promise<AxiosResponse> => {
-  const hostname = window.host.hostname || 'NO_HOSTNAME_FOUND';
+  const hostname = process.env.COMPUTER_NAME || 'NO_COMPUTER_NAME_FOUND';
   const response = await api.post('/ps/insert_digiturno', {
     hostname: hostname,
     ccTecnico: turn.userDocument,
