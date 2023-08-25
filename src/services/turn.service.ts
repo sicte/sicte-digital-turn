01368e0a7a0bc5308fe4e583b8d0ecfc200e82f6
KybalionX <nicolaseduardoflorez1@gmail.com>
2023-08-24 20:16:11 -0500

    feat(services): changes url in services

--- src/services/turn.service.ts
+++ src/services/turn.service.ts
@@ -4,7 +4,7 @@ import { Turn } from 'src/models/turn.model';
 
 export const insertNewTurn = async (turn: Turn): Promise<AxiosResponse> => {
   const hostname = process.env.COMPUTER_NAME || 'NO_COMPUTER_NAME_FOUND';
-  const response = await api.post('/ps/insert_digiturno', {
+  const response = await api.post('/insert_digiturno', {
     hostname: hostname,
     ccTecnico: turn.userDocument,
     tramite: turn.type,
